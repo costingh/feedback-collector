0d2651d8bc07966b5a3b0190c3cba8be91364df4
Costin Gheorghe <costingh0702@stud.acs.upb.ro>
2024-09-22 09:58:31 +0300

    [NEW] edit widget modal (select testimonials to share on it). TBD: filters

--- app/(form)/share/edit/[widgetId]/page.tsx
+++ app/(form)/share/edit/[widgetId]/page.tsx
@@ -11,14 +11,18 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { timeAgo } from "@/lib/utils";
 import DisplayTestimonialTags from "@/components/tags/DisplayTestimonialTags";
 import StarsRating from "@/components/stars-rating";
+import TestimonialsList from "@/components/testimonials/TestimonialsList";
+import { SelectTestimonialsToShareModal } from "@/components/testimonials/SelectTestimonialsToShareModal";
 
 const LandingPage = ({ params }: { params: { widgetId: string } }) => {
 	const [isSearchingWidgets, setIsSearchingWidgets] = useState(true);
 	const [widgets, setWidgets] = useState([]);
 	const [startTime, setStartTime] = useState<number | null>(null); // Start time for time spent
 	const [hasInteracted, setHasInteracted] = useState(false); // Track user interaction
-	const [activeSubmenu, setActiveSubmenu] = useState<string>('')
-
+	const [activeSubmenu, setActiveSubmenu] = useState<string>("");
+	const [checkedItems, setChecked] = useState(new Set());
+	const [testimonials, setTestimonials] = useState([]);
+	const [tags, setTags] = useState([]);
 
 	const handleGetAllUserWidgets = useCallback(async () => {
 		setIsSearchingWidgets(true);
@@ -94,32 +98,41 @@ const LandingPage = ({ params }: { params: { widgetId: string } }) => {
 		}
 	};
 
-
-	const [testimonials, setTestimonials] = useState([])
-	const [tags, setTags] = useState([])
-
 	useEffect(() => {
-		const fetchAllData = async () => {
-			try {
-				// setIsSearchingTestimonials(true);
-
-				const [tagsResponse, testimonialsResponse] = await Promise.all([
-					axios.get("/api/tag/get-all-user-tags"),
-					axios.get("/api/testimonials/get-all-user-testimonials"),
-				]);
-
-				console.log(testimonialsResponse?.data?.testimonials)
-				setTags(tagsResponse?.data?.tags || []);
-				setTestimonials(testimonialsResponse?.data?.testimonials || []);
-			} catch (err) {
-				toast.error("An error occurred while retrieving your data!");
-			} finally {
-				// setIsSearchingTestimonials(false);
-			}
-		};
-
 		fetchAllData();
 	}, []);
+
+	const fetchAllData = async () => {
+		try {
+			// setIsSearchingTestimonials(true);
+
+			const [tagsResponse, testimonialsResponse] = await Promise.all([
+				axios.get("/api/tag/get-all-user-tags"),
+				axios.get("/api/testimonials/get-all-user-testimonials"),
+			]);
+
+			console.log(testimonialsResponse?.data?.testimonials);
+			setTags(tagsResponse?.data?.tags || []);
+			setTestimonials(testimonialsResponse?.data?.testimonials || []);
+		} catch (err) {
+			toast.error("An error occurred while retrieving your data!");
+		} finally {
+			// setIsSearchingTestimonials(false);
+		}
+	};
+
+	useEffect(() => {
+		if(widgets?.[0]?.testimonials)
+			setChecked(new Set(widgets[0].testimonials.map(t => t.id)))
+	}, [widgets])
+
+	const refreshData = async () => {
+		await fetchAllData()
+	}
+
+	const isChecked = (id: number) => {
+		return checkedItems.has(id);
+	};
 
 	return (
 		<>
@@ -128,123 +141,64 @@ const LandingPage = ({ params }: { params: { widgetId: string } }) => {
 				className="relative bg-gray-100 min-h-[100vh]"
 			>
 				<div className="hidden h-full md:flex w-[60px] md:flex-col md:fixed md:inset-y-0 z-80 border-r-[1px] border-gray-200 bg-white">
-					<WidgetEditorSidebar activeSubmenu={activeSubmenu} setActiveSubmenu={setActiveSubmenu} />
+					<WidgetEditorSidebar
+						activeSubmenu={activeSubmenu}
+						setActiveSubmenu={setActiveSubmenu}
+					/>
 				</div>
-				<WidgetEditorNav/>
+				<WidgetEditorNav />
 				{isSearchingWidgets ? (
 					<div className="mt-10">
 						<Loader />
 					</div>
 				) : (
-					<>
-						{activeSubmenu == 'select_testimonials' && <div className="bg-white w-[450px] h-[calc(100vh-100px)] pl-[80px]">
-						{testimonials.map((t: any, index: number) => (
-									<>
-										<div
-											key={t.id}
-											className="flex px-4 py-3 rounded-[10px] w-full justify-between cursor:pointer hover:bg-zinc-100 gap-4 mb-4"
-											
-										>
-											<div className="avatar flex flex-col items-start max-w-full w-[350px]">
-												<Avatar>
-													<AvatarImage
-														src={t?.avatar}
-													/>
-													<AvatarFallback>
-														CN
-													</AvatarFallback>
-												</Avatar>
-												<p className="text-zinc-700 text-[14px] font-[600] mb-1 mt-2">
-													{t?.name}
-												</p>
-												{/* <span>{t?.email}</span> */}
-												<span className="text-gray-600 text-[13px] font-[400]">
-													{t?.jobTitle || t?.email}
-												</span>
-											</div>
-											<div className="w-full">
-												{/* <span>{t?.logo}</span> */}
-												{/* <span>{t?.company}</span> */}
-												{/* <span>{t?.website}</span> */}
-												{/* <span>{t?.jobTitle}</span> */}
-												<div className="flex items-center gap-6">
-													<span className="text-gray-400 font-semibold text-[12px]">
-														{timeAgo(t?.createdAt)}
-													</span>
-
-													<DisplayTestimonialTags
-														tags={tags}
-														id={t.id}
-													/>
-												</div>
-
-												<div className="my-3 text-[16px] text-gray-700 font-normal max-w-[700px]">
-													{t?.message}
-												</div>
-
-												<div className="flex items-center">
-													<StarsRating
-														value={Math.ceil(
-															t.stars
-														)}
-														readonly={true}
-													/>
-												</div>
-											</div>
-
-											<div className="w-[350px] flex items-center gap-4 justify-end">
-												{t?.approved ? (
-													<span className="px-2 py-1 rounded-[10px] bg-[#4dff0769] text-[#0d7d01e6] text-[12px] font-semibold cursor-pointer">
-														Approved
-													</span>
-												) : (
-													<span className="px-2 py-1 rounded-[10px] bg-[#ffc10769] text-[#7d5e01e6] text-[12px] font-semibold cursor-pointer">
-														Not Approved
-													</span>
-												)}
-
-												{/* <Checkbox
-													id={`check-${t.id}`}
-													checked={checkedItems.has(
-														t.id
-													)}
-													onClick={() =>
-														setChecked((prev) => {
-															const newCheckedItems =
-																new Set(prev);
-															if (
-																newCheckedItems.has(
-																	t.id
-																)
-															) {
-																newCheckedItems.delete(
-																	t.id
-																); // Uncheck by removing the item from the set
-															} else {
-																newCheckedItems.add(
-																	t.id
-																); // Check by adding the item to the set
-															}
-															return newCheckedItems;
-														})
-													}
-												/> */}
-											</div>
-										</div>
-										<div className="w-full bg-gray-100 h-[1px]"></div>
-									</>
-								))}
-						</div>}
-						{widgets?.[0]?.testimonials && <DisplayWidget widget={widgets?.[0]} />}
-						{!widgets?.[0] && <div>An error occured</div> }
-						{!widgets?.[0]?.testimonials?.length && <div className='w-full h-full flex items-center justify-center'>
-							<div className='flex flex-col items-center justify-center max-w-lg text-center'>
-								<h1 className='text-black font-700 text-[20px] mb-2'>Oops, looks like you didnt link any testimonials</h1>
-								<p className='text-gray-700 text-[16px] mb-2'>You cant share this "Carousel" like this, so please select some testimonials to include them in the carousel.</p>
-								<span className='text-gray-500 text-[14px]'>Very important: only "approved" testimonials will appear here.</span>
+					<div className="">
+						{/* {activeSubmenu == "select_testimonials" && (
+							<div className="bg-white w-[450px] h-[calc(100vh-100px)] pl-[80px] p-5">
+								<TestimonialsList
+									testimonials={testimonials}
+									tags={tags}
+									isChecked={isChecked}
+									setChecked={setChecked}
+									checkedItems={checkedItems}
+								/>
 							</div>
-						</div> }
-					</>
+						)} */}
+						<SelectTestimonialsToShareModal 
+							isOpened={activeSubmenu == "select_testimonials"} 
+							handleClose={() => setActiveSubmenu('')} 
+							testimonials={testimonials}
+							tags={tags}
+							isChecked={isChecked}
+							setChecked={setChecked}
+							checkedItems={checkedItems}
+							widgetId={widgets?.[0].id}
+							refreshData={refreshData}
+						/>
+						{widgets?.[0]?.testimonials && (
+							<DisplayWidget widget={widgets?.[0]} />
+						)}
+						{!widgets?.[0] && <div>An error occured</div>}
+						{!widgets?.[0]?.testimonials?.length && (
+							<div className="w-full h-full flex items-center justify-center">
+								<div className="flex flex-col items-center justify-center max-w-lg text-center">
+									<h1 className="text-black font-700 text-[20px] mb-2">
+										Oops, looks like you didnt link any
+										testimonials
+									</h1>
+									<p className="text-gray-700 text-[16px] mb-2">
+										You cant share this "Carousel" like
+										this, so please select some testimonials
+										to include them in the carousel.
+									</p>
+									<span className="text-gray-500 text-[14px]">
+										Very important: only "approved"
+										testimonials will appear here.
+									</span>
+								</div>
+							</div>
+						)}
+					</div>
 				)}
 			</div>
 		</>
---
+++ components/testimonials/SelectTestimonialsToShareModal.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+import { toast } from "sonner";
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogDescription,
+	DialogFooter,
+} from "@/components/ui/dialog";
+import TestimonialsList from "./TestimonialsList";
+import { LoadingSpinner } from "../LoadingSpinner";
+
+export const SelectTestimonialsToShareModal = ({
+	isOpened,
+	handleClose,
+	testimonials,
+	isChecked,
+	setChecked,
+	tags,
+	checkedItems,
+    widgetId,
+    refreshData
+}: {
+	isOpened: boolean;
+	handleClose: any;
+	testimonials: any;
+	isChecked: any;
+	setChecked: any;
+	tags: any;
+	checkedItems: any;
+    widgetId: string;
+    refreshData?: any
+}) => {
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleUpdate = async () => {
+        setIsLoading(true)
+
+        try {
+            const response = await axios.post("/api/widgets/update", {
+                data: {
+                    widgetId: widgetId,
+                    testimonialsIds: Array.from(checkedItems),
+                },
+            });
+    
+            const resp = response?.data?.updatedWidget;
+    
+            if(resp) {
+                refreshData && refreshData()
+                handleClose()
+                toast.success('Success')
+            } else {
+                toast.error('Error')
+            }
+        } catch(error) {
+            console.log(error)
+            toast.error(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+	return (
+		<Dialog open={isOpened} onOpenChange={handleClose}>
+			<DialogContent className="max-w-[980px]">
+				<DialogHeader>
+					<DialogTitle className="flex justify-between items-between flex-col gap-y-4 pb-2">
+						<div className="flex items-center gap-x-2 font-bold text-xl">
+							Select testimonials
+						</div>
+					</DialogTitle>
+					<DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
+						<TestimonialsList
+							testimonials={testimonials}
+							tags={tags}
+							isChecked={isChecked}
+							setChecked={setChecked}
+							checkedItems={checkedItems}
+						/>
+					</DialogDescription>
+				</DialogHeader>
+				<DialogFooter>
+					<div onClick={handleUpdate} className="text-center py-[10px] rounded-[8px] bg-[#000] text-[#eee] w-full cursor-pointer text-[14px] font-semibold mt-10 hover:opacity-80 max-w-[200px]">
+						{!isLoading ? (
+							"Apply changes"
+						) : (
+							<div className="flex items-center justify-center">
+								<LoadingSpinner />
+							</div>
+						)}
+					</div>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+};
