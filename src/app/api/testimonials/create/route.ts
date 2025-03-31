10f966b6732fddc6c48bbcee0fad74ab5675948b
Costin Gheorghe <cgheorghe0702@stud.acs.upb.ro>
2025-03-31 21:05:21 +0300

    [FIX] check for email constraint only when email field is specified

--- src/app/api/testimonials/create/route.ts
+++ src/app/api/testimonials/create/route.ts
@@ -1,35 +1,20 @@
-import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import {client} from "@/lib/prisma";
 
 export async function POST(req: Request) {
     try {
-        const { userId } = auth();
         const body = await req.json();
         const { data } = body;
 
-        if (!userId) {
-            return new NextResponse("Unauthorized", { status: 401 });
-        }
-
-        console.log(data);
-
-        // const freeTrial = await checkApiLimit();
-        // const { isValid: isPro, planType } = await checkSubscription();
-
-        // if (!freeTrial && !isPro) {
-        //     return new NextResponse(
-        //         "Free trial has expired. Please upgrade to pro.",
-        //         { status: 403 }
-        //     );
-        // }
-
-        const existingReview = await client.formResponse.findFirst({
-            where: { email: data.email, formId: data.formId },
-        });
-
-        if (existingReview) {
-            return NextResponse.json({ result: null, error: "A review was already submitted with this email." });
+        // dont allow an email to be used more than once
+        if(data.email) {
+            const existingReview = await client.formResponse.findFirst({
+                where: { email: data.email, formId: data.formId },
+            });
+    
+            if (existingReview) {
+                return NextResponse.json({ result: null, error: "A review was already submitted with this email." });
+            }
         }
 
         const result = await client.formResponse.create({
